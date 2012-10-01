ef7169161ef5b52e758392dc4b8e3f974cabc1d8
Marak <marak.squires@gmail.com>
2012-10-01 00:31:34 -0700

    [admin] [view] Added dependencies list to resource view

--- admin/view/resource.js
+++ admin/view/resource.js
@@ -6,6 +6,12 @@ module['exports'] = function (options, callback) {
   $('.description').html(options.resource.schema.description);
   $('.methods').html(layout.controls.list.present({ items: options.methods, root: '/admin/resources/' + options.resource.name + '/' }));
 
+  if(Object.keys(options.resource.dependencies).length > 0) {
+    $('.dependencies').html(layout.controls.list.present({ items: options.resource.dependencies, root: 'http://npmjs.org/package/' }));
+  } else {
+    $('.depsRow').remove();
+  }
+
   return $.html();
 
 }
