6d8c88b0a62323bfca4165b005541d3753d08dcb
Marak Squires <marak.squires@gmail.com>
2012-10-01 23:43:02 -0700

    [refactor] Removed noc resource. Added node.sh method for executing shell scripts remotely over ssh.

--- node/index.js
+++ node/index.js
@@ -44,4 +44,105 @@ node.property('lastSeen', {
   "type": "string"
 });
 
+node.method('sh', sh, {
+  "description": "execute shell scripts on a remote node via SSH",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "host": {
+          "type": "string",
+          "default": "0.0.0.0"
+        },
+        "recipe": {
+          "description": "path to the shell script to run remotely",
+          "type": "string",
+          "default": __dirname + "/recipes/ls-test"
+        }
+      }
+    },
+    "callback": {
+      "type": "function"
+    }
+  }
+});
+
+var spawn = require('child_process').spawn,
+    fs = require('fs'),
+    sys = require('sys');
+
+function sh (options, callback) {
+
+  options.username = "root";
+
+  //
+  // Read in shell script recipe file
+  //
+  fs.readFile(options.recipe, function (err, data) {
+
+    if (err) {
+      return callback(err);
+    }
+
+    var loggedIn = false,
+    lines,
+    commands = [],
+    ssh;
+
+   //
+   // Turn recipe commands into new line delimited array
+   //
+   lines = data.toString().split('\n');
+
+   //
+   // Remove any line that is empty or starts with a comment
+   //
+   lines.forEach(function(line){
+     if(line.substr(0, 1) !== '#' && line.length > 0) {
+       commands.push(line);
+     }
+   });
+
+   //
+   // Join the commands back into bash commands using && to concat each command
+   // This creates a long string of bash commands connected by &&,
+   // which are executed sequentially
+   //
+   commands = commands.join(' && ');
+
+   //
+   // Spawn SSH binary as child process
+   //
+   ssh = spawn('ssh', ['-l' + options.username, options.host, commands]);
+
+   //
+   // When the SSH binary exits, execute the callback
+   //
+   ssh.on('exit', function (code, signal) {
+     callback(null, {
+       code: code,
+       signal: signal
+     })
+   });
+
+   ssh.stderr.on('data', function (err) {
+     process.stdout.write(err);
+   });
+
+   ssh.stdout.on('data', function (out) {
+     process.stdout.write(out);
+     if (!loggedIn) {
+       var stdin = process.openStdin();
+       stdin.on('data', function (chunk) {
+         ssh.stdin.write(chunk);
+       });
+     }
+     loggedIn = true;
+   });
+
+
+ });
+
+}
+
 exports.node = node;
