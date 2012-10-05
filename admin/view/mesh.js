77b3fd11f5890c6124e3e8931bec2d8604da6d7d
Marak <marak.squires@gmail.com>
2012-10-05 13:48:26 -0700

    [admin] Added servers list to mesh view

--- admin/view/mesh.js
+++ admin/view/mesh.js
@@ -1,9 +1,20 @@
-var layout = require('./layout');
+var layout = require('./layout'),
+resource = require('resource');
 
 module['exports'] = function (options, callback) {
 
   var $ = this.$;
-
-  return $.html();
+  var output = '';
+  resource.node.find({ role: 'server' }, function(err, results){
+    results.forEach(function(record){
+      output += ('<tr>'
+             +     '<td><a href="http://' + record.id +'">' + record.id + '</a></td>'
+             +     '<td>' + record.status + '</td>'
+             +     '<td>' + record.lastSeen + '</td>'
+             +   '</tr>');
+    });
+    $('.records').html(output);
+    callback(null, $.html())
+  });
 
 }
