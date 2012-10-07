3f6120a6ae2874cf88986fbae4fe052f77acef58
Marak Squires <marak.squires@gmail.com>
2012-10-06 18:33:50 -0700

    [replicator] Added new methods, replicator.checkout and replicator.log. Now using env.HOME for replicator home. Decoupled checkout from push.

--- replicator/index.js
+++ replicator/index.js
@@ -75,6 +75,162 @@ replicator.method('pull', pull, {
   }
 });
 
+replicator.method('log', _log, {
+  "description": "shows past commit history of replicator"
+});
+
+
+function _log (callback) {
+
+  var spawn = require('child_process').spawn;
+  var out = [],
+      git = spawn('git', [ 'log', '--numstat' ], { cwd: process.cwd() });
+
+  git.stdout.on('data', function (data) {
+    out.push(data.toString());
+  });
+
+  git.stderr.on('data', function (data) {
+    throw e;
+  });
+
+  git.on('exit', function (code) {
+    if (out.length) {
+      parse(out.join(''), callback);
+    }
+    else {
+      throw new Error('no logs given');
+    }
+  });
+
+  function parse(logs, callback) {
+    var commits = [],
+        thisCommit = {},
+        mode = 'commit';
+
+    function unshift() {
+      thisCommit.msg = thisCommit.msg.join('\n');
+      commits.unshift(thisCommit);
+    }
+
+    logs = logs.split('\n');
+
+    logs.forEach(function (l, i) {
+      if (mode === 'commit') {
+        if (l.substr(0, 6) === 'commit') {
+          init();
+          mode = 'header';
+          return;
+        }
+        throw parseError('expected `commit`');
+      }
+      else if (mode === 'header') {
+        if (l === '') {
+          mode = 'msg';
+          return;
+        }
+        header();
+      }
+      else if (mode === 'msg') {
+        if (l.substr(0, 4) === '    ') {
+          return msg();
+        }
+        if (l === '') {
+          mode = 'numstat';
+          return;
+        }
+        throw parseError('unexpected non-indented block in msg');
+      }
+      else if (mode === 'numstat') {
+        if (l.substr(0, 6) === 'commit') {
+          unshift();
+          init();
+          mode = 'header';
+          return;
+        }
+        if (l === '') {
+          unshift();
+          mode = 'commit';
+          return;
+        }
+        numstat();
+      }
+      return;
+
+      function init() {
+        thisCommit = {
+          commit: l.split(' ')[1],
+          msg: [],
+          plus: 0,
+          minus: 0
+        };
+      }
+
+      function header() {
+        try {
+          var kvp = l.split(':'),
+              key = kvp[0].toLowerCase(),
+              val = kvp[1].trim();
+
+          if (key == 'date') {
+            console.log(val);
+            val = new Date(val);
+          }
+
+          thisCommit[key] = [val];
+        }
+        catch (err) {
+          throw parseError('malformed header format');
+        }
+      }
+
+      function msg() {
+        thisCommit.msg.push(l.substr(4));
+      }
+
+      function numstat() {
+        // way too lazy to do a regexp or anything clever.
+        var row = l.split('\t');
+
+        thisCommit.plus += parseInt(row[0], 10);
+        thisCommit.minus += parseInt(row[1], 10);
+      }
+
+      function parseError(msg) {
+        return new Error(util.format(
+          'git logs parse error: %s\non line %d: `%s`',
+          msg || '(unspecified)', i, l
+        ));
+      }
+    });
+
+    // push on last one
+    //unshift();
+    commits = commits.reverse();
+    callback(null, commits);
+  }
+
+}
+
+replicator.method('checkout', function(callback){
+  // TODO: add ability to check out specific sha instead of master
+  console.log('checking out latest commit...');
+  var exec = require('child_process').exec;
+  var _command = "git --work-tree=" + process.env.HOME + "/big/ checkout -f";
+  var checkout = exec(_command, { cwd: '/tmp/repos/big/' },
+    function (err, stdout, stderr) {
+      console.log(stdout, stderr);
+      if (err) {
+        // TODO: do something meaningful with the error
+        console.log('exec error: ' + err);
+      } else {
+        console.log('checked out latest commit to: ~/big/');
+      }
+      console.log('restart needed to update');
+      callback(err, true);
+  });
+});
+
 replicator.method('listen', listen, {
   "description": "starts a listening replicator service capable of recieving big push requests"
 });
@@ -89,20 +245,6 @@ function listen () {
   repos.on('push', function (push) {
       console.log('push ' + push.repo + '/' + push.commit + ' (' + push.branch + ')');
       push.accept();
-      console.log('checking out latest commit...');
-      var exec = require('child_process').exec;
-      var _command = "git --work-tree=/root/big/ checkout -f";
-      var checkout = exec(_command, { cwd: '/tmp/repos/big/' },
-        function (error, stdout, stderr) {
-          if (error !== null) {
-            // TODO: do something meaningful with the error
-            console.log('exec error: ' + error);
-          } else {
-            console.log('checked out lastest commit to: /root/big/');
-          }
-          console.log('restart needed to update');
-          console.log('exiting process... ( there should be a process monitor watching this )')
-      });
   });
 
   repos.on('fetch', function (fetch) {
@@ -118,6 +260,7 @@ function listen () {
   });
 
 };
+
 
 // pushes current big instance to another
 function push (options, callback) {
