c6050da78aa6bc36378da96595375e6a4294e425
Marak <marak.squires@gmail.com>
2012-10-06 18:34:39 -0700

    [admin] Added replication history to replicator view

--- admin/view/replicator.js
+++ admin/view/replicator.js
@@ -1,9 +1,15 @@
-var layout = require('./layout');
+var layout = require('./layout'),
+    resource = require('resource');
 
 module['exports'] = function (options, callback) {
 
   var $ = this.$;
-
-  return $.html();
+  resource.replicator.log(function(err, results){
+    console.log(err, results);
+    results.forEach(function(result){
+      $('table').append('<tr><td>'+result.author+'</td><td>'+result.date+'</td><td>'+result.msg+'</td></tr>');
+    });
+    callback(null,  $.html());
+  });
 
 }
