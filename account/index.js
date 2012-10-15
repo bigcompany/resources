1cf3ff7778bf4abc8c1645c73cb19766078c1fea
Marak Squires <marak.squires@gmail.com>
2012-10-14 19:27:40 -0700

    [dist] Rename 'user' resource to 'account'

---
+++ account/index.js
@@ -0,0 +1,107 @@
+var resource = require('resource'),
+    account = resource.define('account');
+
+account.schema.description = "for managing accounts"
+
+account.property('email', {
+  "type": "string",
+  "format": "email",
+  "required": true
+});
+
+account.property('password', {
+  "type": "string",
+  "format": "password"
+});
+
+account.property('status', {
+  "description": "the current status of the account",
+  "type": "string",
+  "enum": ["new", "active", "inactive", "disabled"],
+  "default": "new"
+});
+
+account.property('token', {
+  "description": "unique access token for the account. used for account confirmations and password resets",
+  "type": "string",
+  "private": true,
+  "default": ""
+});
+
+account.method('confirm', confirm, {
+  "description": "confirms a new account based on access token",
+  "properties": {
+    "token": {
+      "type": "string",
+      "description": "access token",
+      "required": true,
+      "message": "access token is required to confirm account"
+    }
+  }
+});
+
+account.method('reset', reset, {
+  "description": "resets access token for account",
+  "properties": {
+    "email": {
+      "type": "string",
+      "format": "email",
+      "required": true
+    }
+  }
+});
+
+account.method('auth', reset, {
+  "description": "checks accountname and password for a account ( auth check )",
+  "properties": {
+    "accountname": {
+      "type": "string",
+      "required": true
+    },
+    "password": {
+      "type": "string",
+      "required": true
+    }
+  }
+});
+
+account.before('create', function(_account, next){
+  //
+  // Generate a new UUID for the account's access token
+  //
+  _account.token = resource.uuid();
+  next(null, _account)
+});
+
+function reset (email, callback) {
+  account.find({ email: email }, function(err, _account){
+    if (err) {
+      return callback(err);
+    }
+    if(_account.length === 0) {
+      return callback(new Error('could not find any accounts with email ' + email));
+    }
+    _account[0].token = resource.uuid();
+    _account[0].save(function(err, result){
+      if (err) {
+        return callback(err);
+      }
+      return callback(null, result.token);
+    });
+  })
+};
+
+function confirm (token, callback) {
+  account.find({ token: token }, function(err, _account){
+    if (err) {
+      return callback(err);
+    }
+    if(_account.length === 0) {
+      return callback(new Error('could not find any accounts with token ' + token));
+    }
+    _account[0].status = "active";
+    _account[0].save(callback);
+  })
+}
+
+exports.account = account;
