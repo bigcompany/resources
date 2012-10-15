c4f07e76d2f6750ebcfa8403227eac2ff66cc9a6
Marak Squires <marak.squires@gmail.com>
2012-10-14 21:50:50 -0700

    [admin] Added view for hooks

---
+++ admin/view/hooks.js
@@ -0,0 +1,35 @@
+var layout = require('./layout'),
+    resource = require('resource');
+
+module['exports'] = function (options, callback) {
+
+  var $ = this.$;
+  
+  var methods = {};
+  
+  for (var r in resource.resources) {
+    console.log(r)
+    for (var m in resource.resources[r].methods) {
+      $('#if').append('<option>' + r + '::' + m + '</option>');
+      $('#then').append('<option>' + r + '::' + m + '</option>');
+      $('#before').append('<option>' + r + '::' + m + '</option>');
+      $('#run').append('<option>' + r + '::' + m + '</option>');
+
+    }
+  }
+  
+  resource.resources.hook.all(function(err, results){
+    results.forEach(function(item){
+      $('.table').append('<tr><td>' + eventLink(item.if) + '</td><td>' + eventLink(item.then) + '</td></tr>')
+    });
+    callback(null,  $.html());
+  });
+
+}
+
+function eventLink (event) {
+  var arr = event.split('::'),
+      _resource = arr[0],
+      _method   = arr[1];
+  return '<a href ="/admin/resources/' + _resource + '/' + _method + '">' + event + '</a>'
+}
