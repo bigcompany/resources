0ffc300c16444b983ff86c5c939d509765d2d19b
Marak Squires <marak.squires@gmail.com>
2012-10-16 11:16:51 -0700

    [admin] Style and design updates to various views

--- admin/view/replicator.js
+++ admin/view/replicator.js
@@ -4,6 +4,18 @@ var layout = require('./layout'),
 module['exports'] = function (options, callback) {
 
   var $ = this.$;
+  
+  /*
+  resource.replicator.all(function(err, results){
+    console.log(err, results);
+    results.forEach(function(result){
+      $('table').append('<tr><td>'+result.date+'</td><td>'+result.msg+'</td><td>'+result.author+'</td></tr>');
+    });
+    callback(null,  $.html());
+  });
+  */
+  
+  
   resource.replicator.log(function(err, results){
     console.log(err, results);
     results.forEach(function(result){
