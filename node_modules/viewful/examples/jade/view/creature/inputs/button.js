0a5ba7f302a760c79c0ce035a963ed7c49cd768e
Marak <marak.squires@gmail.com>
2012-11-13 13:53:42 +0530

    [dist] Removed vendored viewful

--- node_modules/viewful/examples/jade/view/creature/inputs/button.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (options, callback) {
-  // Remark: We get an isomorphic querySelectorAll poly-fill for free!
-  console.log('sup')
-  var $ = this.$;
-  $('#thebutton').click(function(){
-    alert('I am alert!');
-  })
-}
