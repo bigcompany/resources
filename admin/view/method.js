d5029239438fb5b1004116f67b95e07d89301d64
Marak Squires <marak.squires@gmail.com>
2012-11-26 13:06:46 +0530

    [admin] [fix] Gracefully handle invalid method names passed to method presenter

--- admin/view/method.js
+++ admin/view/method.js
@@ -8,11 +8,22 @@ module['exports'] = function (options, callback) {
       $ = this.$,
       view = this;
 
+  if (typeof method === "undefined") {
+    $('.codeHolder').remove();
+    $('.schemaHolder').remove();
+    $('h1').html('invalid method: ' + options.method);
+    return callback(null, $.html());
+  }
+
+  $('.back').attr('href', '/admin/resources/' + options.resource);
+  $('.schema').html(view.parent.schema.present({ schema: method.schema }));
+
   $('h1').html(options.resource + ' ' + method.name);
   $('.description').html(method.schema.description);
   // $('.methods').html(view.parent.layout.controls.list.present({ items: r.methods, root: '/admin/resources/foo/' }));
   $('.method').html(hl(method.unwrapped.toString()));
-  var form = resource.forms.generate(options, function(err, str){
+
+  var form = resource.forms.generate(options, function(err, str) {
     if(options.data) {
      $('.schemaHolder').remove();
      $('.codeHolder').remove();
@@ -21,7 +32,5 @@ module['exports'] = function (options, callback) {
     callback(null, $.html())
   });
 
-  $('.back').attr('href', '/admin/resources/' + options.resource);
-  $('.schema').html(view.parent.schema.present({ schema: method.schema }));
 
 }
