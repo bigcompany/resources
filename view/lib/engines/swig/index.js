cd69b50d17d50a7b73cacc4c33c7b1c9d933bf8b
Marak <marak.squires@gmail.com>
2012-12-08 23:13:49 +0530

    [view] [minor] Code formatting

--- view/lib/engines/swig/index.js
+++ view/lib/engines/swig/index.js
@@ -4,7 +4,7 @@
 var swig = require('swig');
 
 exports.render = function (view, data) {
-  var html= swig.compile(view.template)(data);
+  var html = swig.compile(view.template)(data);
   return html;
 };
 
