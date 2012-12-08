ad143fecfa40121394f9ae5941ab8a23fc123581
Marak Squires <marak.squires@gmail.com>
2012-12-08 23:05:54 +0530

    [http] Added virtualhost support. Partial commit to share.

--- http/index.js
+++ http/index.js
@@ -1,4 +1,5 @@
 var resource  = require('resource'),
+    path = require('path'),
     http = resource.define('http');
 
 http.schema.description = "provides an HTTP server API";
@@ -17,7 +18,7 @@ http.property("host", {
 
 http.property("root", {
   "type": "string",
-  "default": __dirname + '/public'
+  "default": process.cwd() + '/public' // TODO: smarter detection of process.cwd() versus __dirname?
 });
 
 http.method('listen', listen, {
@@ -53,6 +54,46 @@ function listen (options, callback) {
 
   var app = express();
 
+  //
+  // Basic virtual host support
+  //
+  /* TODO: finish virtual host resource
+  if (resource.virtualhost) {
+    resource.logger.info('using virtual hosts');
+    app.use(function(req, res, next){
+      var host = req.headers.host.split(':');
+      host = host[0];
+      resource.virtualhost.find({ host: host }, function(err, results) {
+        if (err || results.length === 0) {
+          return next();
+        }
+        connect.static(path.resolve(results[0].path))(req, res, next);
+      });
+    });
+  }
+  */
+
+  /* TODO: finish resource.view middleware
+  app.use(function (req, res, next) {
+  });
+  var view = resource.view.create({ path: process.cwd() + '/view'});
+  view.load();
+
+  //console.log(view);
+  //console.log(req.url);
+  var name = "index";
+  if (req.url === "/") {
+    name = "index";
+  }
+  if(typeof view[name] === "undefined") {
+    return next();
+  }
+  var str = view[name].render({});
+  str = view[name].present({}, function (err, str) {
+    res.end(str);
+  });
+  */
+
   if(typeof options.root !== 'undefined') {
     app
       .use(connect.static(options.root))
@@ -73,24 +114,26 @@ function listen (options, callback) {
     }));
   }
 
-   http.server = server = require('http').createServer(app).listen(options.port, options.host, function (){
-      callback(null, server);
-   });
 
-   app.get('/', function (req, res){
-     res.end('home');
-   });
+  http.server = server = require('http').createServer(app).listen(options.port, options.host, function () {
+   callback(null, server);
+  });
 
-   //
-   // Bind app instance to http resource
-   //
-   http.app = app;
+  app.get('/', function (req, res){
+    res.end('home');
+  });
+
+  //
+  // Bind app instance to http resource
+  //
+  http.app = app;
+  http.server = server;
 
 }
 
 exports.http = http;
 
 exports.dependencies = {
-  "connect": "*",
-  "express": "*"
+  "connect": "2.7.0",
+  "express": "2.5.11"
 };
