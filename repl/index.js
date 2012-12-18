af6a6f14bf07aa58decaa55e1379c39f344b37d6
Marak <marak.squires@gmail.com>
2012-12-19 00:52:07 +0530

    [repl] [ux] Use `big` context instead of `resource`. Updated messaging.

--- repl/index.js
+++ repl/index.js
@@ -2,10 +2,9 @@ var resource = require('resource'),
 repl = resource.define('repl');
 
 repl.method('start', function() {
-  resource.logger.info('resource repl enabled!');
-  resource.logger.warn('STDIN will now be processed in a read-eval-print-loop');
-  resource.logger.help('hint: try console logging the "resource" object');
-  require('repl').start("> ").context.resource = resource;
+  resource.logger.warn('STDIN will now be processed in a Read-Eval-Print-Loop');
+  resource.logger.help('try console logging the "big" object');
+  require('repl').start("> ").context.big = resource;
 });
 
 exports.repl = repl;
