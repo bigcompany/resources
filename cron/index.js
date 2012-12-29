9fee9ce3d63f718538d480f7aacb0737abb302f6
Marak <marak.squires@gmail.com>
2012-12-29 14:02:20 +0530

    [cron] [fix] Removed process.nextTick call ( fixed in core resource library by https://github.com/bigcompany/resource/commit/44ba095a20cd2e0049d1277cb01be764760ff81e ). Adjusted cron format from five minutes to five seconds.

--- cron/index.js
+++ cron/index.js
@@ -5,7 +5,7 @@ cron.schema.description = "for managing cron jobs";
 
 cron.property("pattern", {
   "type":"string",
-  "default": "*/5 * * * *",
+  "default": "*/5 * * * * *",
   "description": "the pattern of the cron job"
 });
 
@@ -30,14 +30,11 @@ cron.method('run', run);
 // TODO: add test case for using .before and .after methods during a resource definition
 // TODO: remove process.next line ( should not be needed )
 //
-process.nextTick(function(){
-  cron.after('create', function(result){
-    var cronJob = require('cron').CronJob;
-    new cronJob(result.pattern, function() {
-      cron.run(result);
-    }, null, true);
-  });
-  
+cron.after('create', function(result){
+  var cronJob = require('cron').CronJob;
+  new cronJob(result.pattern, function() {
+    cron.run(result);
+  }, null, true);
 });
 
 function start () {
