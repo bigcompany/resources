39f7d871eb2aa4024aea3b499b48d5cfbb0ffb2e
Marak Squires <marak.squires@gmail.com>
2013-01-07 19:15:46 +0530

    [refactor] Moved http virtualhost code into virtualhost resource

--- virtualhost/index.js
+++ virtualhost/index.js
@@ -6,4 +6,24 @@ virtualhost.schema.description = "provides virtual hosts";
 virtualhost.property('host');
 virtualhost.property('path');
 
+virtualhost.middle = function(req, res, next) {
+  var connect = require('connect'),
+      path = require('path'),
+      host = req.headers.host.split(':');
+  host = host[0];
+  resource.virtualhost.find({ host: host }, function (err, results) {
+    if (err || results.length === 0) {
+      resource.virtualhost.all(function (err, hosts) {
+        return res.end('unknown host: ' + host + ' \n' + 'available hosts are ' + JSON.stringify(hosts, true, 2));
+      });
+    } else {
+      connect.static(path.resolve(process.cwd() + results[0].path))(req, res, next);
+    }
+  });
+};
+
 exports.virtualhost = virtualhost;
+
+exports.dependencies = {
+  "connect": "2.7.1",
+};
