42b2975dc71f917792cc0a48662087cd7dca1c50
Marak Squires <marak.squires@gmail.com>
2013-01-13 19:53:27 +0530

    [minor] Added missing descriptions to repl and twitter resources

--- repl/index.js
+++ repl/index.js
@@ -1,6 +1,8 @@
 var resource = require('resource'),
 repl = resource.define('repl');
 
+repl.schema.description = "enables an interactive Read-Eval-Print-Loop ( REPL )";
+
 repl.method('start', function() {
   resource.logger.warn('STDIN will now be processed in a Read-Eval-Print-Loop');
   resource.logger.help('try console logging the "big" object');
