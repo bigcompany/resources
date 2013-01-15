f17fcf3cb9714e5d199110372cd6c7f9fc4c1f3d
Joshua Holbrook <josh.holbrook@gmail.com>
2013-01-15 19:36:24 +0530

    [node] [fix] [minor] Fix typo in code comment

--- node/index.js
+++ node/index.js
@@ -91,7 +91,7 @@ node.method('sh', sh, {
 
 //
 // Before any nodes are created...
-// If they do not have a name, create a new named based on the host port tuple
+// If they do not have a name, create a new name based on the host port tuple
 //
 node.before('create', function(data, next){
   if(typeof data.name !== 'undefined') {
