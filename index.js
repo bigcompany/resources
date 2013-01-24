53f34f9d23d069951224f84cb7dad9f975fc8472
Marak <marak.squires@gmail.com>
2013-01-24 14:12:46 +0530

    [api] Require all `/resources/` when `resources` module is required

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 //
-// resources
+// A collection of useful resources
 //
 var resources = exports;
+
+var resource = require('resource'),
+    fs = require('fs');
+
+//
+// Read "/resources/" directory
+//
+var _resources = fs.readdirSync(__dirname);
+
+//
+// Filter out any potential non-resource files / folders
+//
+_resources = _resources.filter(function (val) {
+  if (["index.js", "package.json", "node_modules", ".git", ".DS_Store", ".gitignore", "README.md"].indexOf(val) !== -1) {
+    return false;
+  }
+  return true;
+});
+
+//
+// For every resource, attempt to require it
+//
+_resources.forEach(function(r){
+  resources[r] = require('./' + r);
+});
