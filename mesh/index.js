4434e2b380b28d4dded3dcfe8df92b24062159e4
Marak Squires <marak.squires@gmail.com>
2013-01-26 09:19:07 +0530

    [mesh] Cleaning up mesh code

--- mesh/index.js
+++ mesh/index.js
@@ -10,6 +10,9 @@ resource.use('node', { datasource: 'fs' });
 resource.use('system');
 
 /*
+
+TODO: mesh events should bubble up to the mesh resource itself
+
 var EventEmitter = require('eventemitter2').EventEmitter2;
 
 var ee = new EventEmitter({
@@ -17,12 +20,12 @@ var ee = new EventEmitter({
   delimiter: '::', // the delimiter used to segment namespaces
   maxListeners: 20, // the max number of listeners that can be assigned to an event
 });
-
 mesh.emit = ee.emit;
 mesh.on = ee.on;
 mesh.onAny = ee.onAny;
 
 */
+
 
 mesh.method('connect', connect, {
   "description": "connect to the big mesh",
@@ -100,7 +103,7 @@ function downlink (socket, callback) {
     //
     //msg.payload.host = socket.remoteAddress.host;
     //msg.payload.port = socket.remoteAddress.port;
-    resource.mesh.emit(msg.event, msg.payload, false)
+    resource.emit(msg.event, msg.payload, false)
   });
 
   socket.on('disconnect', function(data){
@@ -133,7 +136,7 @@ function uplink (options, callback) {
   mesh.client.on('message', function(data){
     var msg = JSON.parse(data);
     console.log('uplink sending', msg)
-    resource.mesh.emit(msg.event, msg.payload, false)
+    resource.emit(msg.event, msg.payload, false)
   })
 
   //
