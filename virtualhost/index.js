c4f607e533e5ac1ee3052bf3f6b85089e91ae94e
Joshua Holbrook <josh.holbrook@gmail.com>
2013-01-29 12:32:27 +0530

    [virtualhost] If a matching virtualhost is not found, log a warning and call the next middleware

--- virtualhost/index.js
+++ virtualhost/index.js
@@ -6,6 +6,8 @@ virtualhost.schema.description = "provides virtual hosts";
 virtualhost.property('host');
 virtualhost.property('path');
 
+virtualhost.persist('memory');
+
 virtualhost.middle = function(req, res, next) {
   var connect = require('connect'),
       path = require('path'),
@@ -14,7 +16,9 @@ virtualhost.middle = function(req, res, next) {
   resource.virtualhost.find({ host: host }, function (err, results) {
     if (err || results.length === 0) {
       resource.virtualhost.all(function (err, hosts) {
-        return res.end('unknown host: ' + host + ' \n' + 'available hosts are ' + JSON.stringify(hosts, true, 2));
+        resource.logger.warn('unknown host: ' + host);
+        resource.logger.warn('available hosts are ' + JSON.stringify(hosts, true, 2));
+        next();
       });
     } else {
       connect.static(path.resolve(process.cwd() + results[0].path))(req, res, next);
