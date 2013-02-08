ba6ecceea0214a4f172f849f7d3247489eb9da39
babang <baban.star@yahoo.com>
2013-02-08 11:17:13 +0530

    [calculator] Moved all methods to resource method

--- calculator/index.js
+++ calculator/index.js
@@ -8,89 +8,375 @@
 //
 // define the main calculator object
 //
-var calc = {};
 
 
-// resource = require("resource");
-// calc = resource.define("calc");
+var resource = require("resource"),
+ calc = resource.define("calculator");
+
+calc.schema.description = "a basic calculator";
 
 //
 // define the addition method
 //
-calc.add = function (a, b) {
-  return a + b; 
+var add = function (options, callback) {
+  var result = options.a + options.b;
+  if (callback) {
+    callback(null, result);
+  } else {
+    return result;
+  }
 }
-// calc.method("add", add);
+calc.method("add", add, {
+  "description": "returns the sum of two numbers",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+          "type": "number"
+        },
+        "b": {
+          "type": "number"
+        }
+       }
+     },
+     "callback": {
+       "type": "function",
+       "required": false
+     }
+   }
+});
 //
 // define the subtraction method
 //
-calc.subtract = function (a, b) {
-  return a - b;
+var subtract = function (options, callback) {
+  var result = options.a - options.b;
+  if (callback) {
+    callback(null, result);
+  } else {
+    return result;
+  }
 }
+calc.method("subtract", subtract, {
+  "description": "returns the difference of two numbers",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+          "type": "number"
+        },
+	"b": {
+          "type": "number"
+        }
+      }
+    },
+    "callback": {
+      "type": "function",
+      "required": false
+    } 
+  }
+});
+
+
 //
 // define the multiplication method
 //
-calc.multiply = function (a, b) {
-  return a * b;
+var multiply = function (options, callback) {
+  var result = options.a * options.b;
+  if (callback) {
+    callback(null, result);
+  } else {
+    return result;
+  }
 }
+calc.method("multiply", multiply, {
+  "description": "returns the product of two numbers",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+          "type": "number"
+        },
+       	"b": {
+          "type": "number"
+        }
+      }
+    },
+    "callback": {
+      "type": "function",
+      "required": false
+    }
+  }
+});
+
 
 //
 // define the division method
 //
-calc.divide = function (a, b) {
-  return a / b;
+var divide = function (options, callback) {
+  var result = options.a / options.b;
+  if (callback) {
+    callback(null, result);
+  } else {
+    return result;
+  }
 }
+calc.method("divide", divide, {
+  "description": "returns the result by dividing two numbers",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+         "type": "number"
+        },
+	"b": {
+          "type": "number"
+        }
+      }
+    },
+    "callback": {
+      "type": "function",
+      "required": false
+    }
+  }
+});
+
+
 //
 // define the power method
 //
-calc.power = function (a, b) {
-  return Math.pow(a, b);
-};
+var power = function (options, callback) {
+  var result = Math.pow(options.a, options.b);
+  if (callback) {
+    callback(null, result);
+  } else {
+    return result;
+  }
+}  
+calc.method("power", power, {
+  "description": "returns the raised power of two numbers",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+         "type": "number"
+        },
+	"b": {
+          "type": "number"
+        }
+      }
+    },
+    "callback": {
+      "type": "function",
+      "required": false
+    }
+  }
+});
+
+
+
 //
 // define the square root method
 //
-calc.squareroot = function (a) {
-  return Math.sqrt(a);
-};
+var squareroot = function (options, callback) {
+  var result = Math.sqrt(options.a);
+  if (callback) {
+    callback(null, result);
+  } else {
+    return result;
+  }
+}
+calc.method("squareroot", squareroot, {
+  "description": "returns the squareoot of number",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+         "type": "number"
+        }
+      }
+    },
+    "callback": {
+      "type": "function",
+      "required": false
+    }
+  }
+});
+
+
 //
 // define the cube root method
 //
-calc.cuberoot = function (a) {
-  return Math.pow(a, 1 / 3);
-};
+var cuberoot = function (options, callback) {
+  var result = Math.pow(options.a, 1/3);
+  if (callback) {
+    callback(null, result);
+  } else {
+    return result;
+  }
+}
+calc.method("cuberoot", cuberoot, {
+  "description": "returns the cuberoot of number",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+         "type": "number"
+        },
+      }
+    },
+    "callback": {
+      "type": "function",
+      "required": false
+    }
+  }
+});
+
+
+
 //
 // define the nthroot method
 //
-calc.nthroot = function (a, b) {
-  return Math.pow(a, 1 / b);
-};
+var nthroot = function (options, callback) {
+  var result = Math.pow(options.a, 1/options.b);
+  if (callback) {
+    callback(null, result);
+  } else {
+    return result;
+  }
+}
+calc.method("nthroot", nthroot, {
+  "description": "returns the nthroot of number",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+          "type": "number"
+        },
+        "b":{
+          "type": "number"
+        }
+      }
+    },
+    "callback": {
+      "type": "function",
+      "required": false
+    }
+  }
+});
+
+
 //
 // define the factorial method
 //
-calc.factorial = function (a) {
+var factorial = function (options, callback) {
  var result = 1;
- for (var i = 1; i <= a; i++) {
-    result = result * i;
+ for (var i = 1; i <= options.a; i++) {
+  result = result * i;
+ }
+ if (callback) {
+   callback(null, result);
+ } else {
+   return result;
+ }
+}
+calc.method("factorial", factorial, {
+  "description": "returns the factorial of number",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+          "type": "number"
+       	}
+      }
+    },
+    "callback": {
+      "type": "function",
+      "required": false
+    }
   }
-  return result;
-};
-//
+});
+
+
+///
 // define the isEven method
 //
-calc.isEven = function (a) {
- var result = a % 2;
- if (result === 1) {
-    return false;
+var isEven = function (options, callback) {
+  var result = options.a % 2;
+  if (result === 1){
+    result = false;
   } else {
-    return true;
+    result = true;
   }
-};
+  if (callback) {
+    callback (null, result);
+  } else {
+    return result;
+  }
+}
+calc.method("isEven", isEven, {
+  "description": "returns the result of number as even",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+          "type": "number"
+        }
+      }
+    },
+    "callback": {
+      "type": "function",
+      "required": false
+    }
+  }
+});
+
+
+
+
 //
 //define the isOdd method
 //
-calc.isOdd = function (a) {
- return !calc.isEven(a);
-};
+var isOdd = function (options, callback) {
+  var result = options.a % 2;
+  if (result === 0){
+    result = false;
+  } else {
+    result = true;
+  }
+  if (callback) {
+    callback (null, result);
+  } else {
+    return result;
+  }
+}
+calc.method("isOdd", isOdd, {
+  "description": "returns the result of number as odd",
+  "properties": {
+    "options": {
+      "type": "object",
+      "properties": {
+        "a":{
+          "type": "number"
+        }
+      }
+    },
+    "callback": {
+      "type": "function",
+      "required": false
+    }
+  }
+});
+
 
 
 console.log(calc);
@@ -114,3 +400,6 @@ console.log(calc.isOdd(9, 3));
 //
 //resource.use("admin"); 
 //resource.admin.listen();
+exports.calculator = calc;
+
+
