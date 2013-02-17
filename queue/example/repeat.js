d74ef82c2414363fe8ea7e771643021ff96550b3
Joshua Holbrook <josh.holbrook@gmail.com>
2013-02-18 01:01:34 +0530

    [queue] Rework examples to use the creature resource

--- queue/example/repeat.js
+++ queue/example/repeat.js
@@ -2,52 +2,35 @@ var resource = require('resource');
 
 var queue = resource.use('queue');
 
-//
-// The queue can call resource methods with call signature (options, callback)
-// Here I just define a new resource as an example.
-//
-var jobs = resource.define('jobs');
+var creature = resource.use('creature');
 
-jobs.method('shortJob', shortJob);
-function shortJob(options, callback) {
-  resource.logger.info('short job: %j', options);
-  setTimeout(callback, 50);
-}
-
-jobs.method('longJob', longJob);
-function longJob(options, callback) {
-  resource.logger.info('long job: %j', options);
-  setTimeout(callback, 1000);
-}
+creature.onAny(function (data) {
+  resource.logger.info(this.event.magenta + ': ', data);
+});
 
 //
-// Create an instance of queue.
+// Create an instance of queue with repeat turned ON
 //
 queue.create({
   interval: 1200,
   concurrency: 2,
   repeat: true
 }, function (err, _queue) {
-  //
-  // Any methods on the queue resource which take a queue instance as the first
-  // argument are bound to the instance after any persistance method which
-  // returns such instances is called. Here, we push "jobs" to the instance
-  // of queue which specify a resource method to call and metadata to call it
-  // with.
+
   //
   // Because repeat is set to true, we only have to push these jobs once.
   //
   _queue.push({
-    method: 'jobs::shortJob',
-    with: { foo: 'bar' }
+    method: 'creature::talk',
+    with: 'Fire!'
   });
   _queue.push({
-    method: 'jobs::longJob',
-    with: { baz: 'quux' }
+    method: 'creature::fire',
+    with: {
+      power: 9001,
+      direction: 'left'
+    }
   });
 
-  // 
-  // On instances, the start method is an alias for the load method.
-  //
   _queue.start();
 });
