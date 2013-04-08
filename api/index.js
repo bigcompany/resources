fac441cad987e4e2229d4686507b2a0f8d85ce9f
Joshua Holbrook <josh.holbrook@gmail.com>
2013-04-08 00:16:50 -0800

    [api] Use async api for view.create in order to avoid first-time-running errors

--- api/index.js
+++ api/index.js
@@ -43,304 +43,310 @@ api.method('listen', listen, {
 
 function listen (options, callback) {
 
-  var resources = options.resources,
-      view = resource.view.create({ path: path.join(__dirname, 'view') });
+  var resources = options.resources;
 
-  view.load();
-
-  resource.http.app.get('/api/:resource', function(req, res) {
-    handle({
-      resource: req.param('resource'),
-      action: "GET"
-    }, req, res);
-  });
-  resource.http.app.post('/api/:resource', function(req, res) {
-    handle({
-      resource: req.param('resource'),
-      action: "POST"
-    }, req, res);
-  });
-  resource.http.app.put('/api/:resource', function(req, res) {
-    handle({
-      resource: req.param('resource'),
-      action: "PUT"
-    }, req, res);
-  });
-
-  resource.http.app.get('/api/:resource/:method', function(req, res) {
-    handle({ 
-      resource: req.param('resource'),
-      method: req.param('method'),
-      action: "GET"
-    }, req, res);
-  });
-  resource.http.app.post('/api/:resource/:method', function(req, res) {
-    handle({ 
-      resource: req.param('resource'),
-      method: req.param('method'),
-      action: "POST"
-    }, req, res);
-  });
-  resource.http.app.put('/api/:resource/:method', function(req, res) {
-    handle({
-      resource: req.param('resource'),
-      method: req.param('method'),
-      action: "PUT"
-    }, req, res);
-  });
-  resource.http.app.del('/api/:resource/:method', function(req, res) {
-    handle({ 
-      resource: req.param('resource'),
-      method: req.param('method'),
-      action: "DELETE"
-    }, req, res);
-  });
-
-  resource.http.app.get('/api/:resource/:id/:method', function(req, res) {
-    handle({ 
-      resource: req.param('resource'),
-      method: req.param('method'),
-      id: req.param('id'),
-      action: "GET"
-    }, req, res);
-  });
-
-  resource.http.app.post('/api/:resource/:id/:method', function(req, res) {
-    handle({ 
-      resource: req.param('resource'),
-      method: req.param('method'),
-      id: req.param('id'),
-      action: "POST"
-    }, req, res);
-  });
-
-  resource.http.app.put('/api/:resource/:id/:method', function(req, res) {
-    handle({ 
-      resource: req.param('resource'),
-      method: req.param('method'),
-      id: req.param('id'),
-      action: "PUT"
-    }, req, res);
-  });
-
-  function handle(options, req, res) {
-
-    var _resource = resource.resources[options.resource],
-        _method = _resource.methods[options.method],
-        isCrudMethod = false;
-
-    var data = {};
-
-    //
-    // Merge query and form data into a common scope
-    //
-    Object.keys(req.query).forEach(function (p) {
-      data[p] = req.query[p];
-    });
-    Object.keys(req.body).forEach(function (p) {
-      data[p] = req.body[p];
-    });
-    if (options.id) {
-      data.id = options.id;
+  resource.view.create({
+    path: path.join(__dirname, 'view')
+  }, function (err, view) {
+    if (err) {
+      return callback(err);
     }
 
-    //
-    // Handle cases where options.method is actually an id and we want to
-    // infer the method from the HTTP method
-    //
-    if (typeof _method === 'undefined' && !options.id) {
-      if (options.method) {
-        data.id = options.method;
-      }
+    view.load();
 
-      if (options.action === 'GET' && options.method) {
-        _method = _resource.methods.get;
-        options.method = 'get';
-      }
-      if (options.action === 'POST' || options.action === 'PUT') {
-        _method = _resource.methods.updateOrCreate;
-        options.method = 'updateOrCreate';
-      }
-      if (options.action === 'DELETE') {
-        _method = _resource.methods.destroy; //?
-        options.method = 'destroy';
-      }
-    }
-
-    //
-    // Methods for which /:resource/:id/:method do not require an implicit
-    // call to r['get'] (ie, "crud methods")
-    //
-    isCrudMethod = [
-      'get',
-      'create',
-      'update',
-      'updateOrCreate',
-      'destroy'
-    ].some(function (method) {
-      return options.method !== method;
+    resource.http.app.get('/api/:resource', function(req, res) {
+      handle({
+        resource: req.param('resource'),
+        action: "GET"
+      }, req, res);
+    });
+    resource.http.app.post('/api/:resource', function(req, res) {
+      handle({
+        resource: req.param('resource'),
+        action: "POST"
+      }, req, res);
+    });
+    resource.http.app.put('/api/:resource', function(req, res) {
+      handle({
+        resource: req.param('resource'),
+        action: "PUT"
+      }, req, res);
     });
 
-    //
-    // Show a list of available methods
-    //
-    if (typeof _method === 'undefined') {
+    resource.http.app.get('/api/:resource/:method', function(req, res) {
+      handle({ 
+        resource: req.param('resource'),
+        method: req.param('method'),
+        action: "GET"
+      }, req, res);
+    });
+    resource.http.app.post('/api/:resource/:method', function(req, res) {
+      handle({ 
+        resource: req.param('resource'),
+        method: req.param('method'),
+        action: "POST"
+      }, req, res);
+    });
+    resource.http.app.put('/api/:resource/:method', function(req, res) {
+      handle({
+        resource: req.param('resource'),
+        method: req.param('method'),
+        action: "PUT"
+      }, req, res);
+    });
+    resource.http.app.del('/api/:resource/:method', function(req, res) {
+      handle({ 
+        resource: req.param('resource'),
+        method: req.param('method'),
+        action: "DELETE"
+      }, req, res);
+    });
+
+    resource.http.app.get('/api/:resource/:id/:method', function(req, res) {
+      handle({ 
+        resource: req.param('resource'),
+        method: req.param('method'),
+        id: req.param('id'),
+        action: "GET"
+      }, req, res);
+    });
+
+    resource.http.app.post('/api/:resource/:id/:method', function(req, res) {
+      handle({ 
+        resource: req.param('resource'),
+        method: req.param('method'),
+        id: req.param('id'),
+        action: "POST"
+      }, req, res);
+    });
+
+    resource.http.app.put('/api/:resource/:id/:method', function(req, res) {
+      handle({ 
+        resource: req.param('resource'),
+        method: req.param('method'),
+        id: req.param('id'),
+        action: "PUT"
+      }, req, res);
+    });
+
+    function handle(options, req, res) {
+
+      var _resource = resource.resources[options.resource],
+          _method = _resource.methods[options.method],
+          isCrudMethod = false;
+
+      var data = {};
+
+      //
+      // Merge query and form data into a common scope
+      //
+      Object.keys(req.query).forEach(function (p) {
+        data[p] = req.query[p];
+      });
+      Object.keys(req.body).forEach(function (p) {
+        data[p] = req.body[p];
+      });
+      if (options.id) {
+        data.id = options.id;
+      }
+
+      //
+      // Handle cases where options.method is actually an id and we want to
+      // infer the method from the HTTP method
+      //
+      if (typeof _method === 'undefined' && !options.id) {
+        if (options.method) {
+          data.id = options.method;
+        }
+
+        if (options.action === 'GET' && options.method) {
+          _method = _resource.methods.get;
+          options.method = 'get';
+        }
+        if (options.action === 'POST' || options.action === 'PUT') {
+          _method = _resource.methods.updateOrCreate;
+          options.method = 'updateOrCreate';
+        }
+        if (options.action === 'DELETE') {
+          _method = _resource.methods.destroy; //?
+          options.method = 'destroy';
+        }
+      }
+
+      //
+      // Methods for which /:resource/:id/:method do not require an implicit
+      // call to r['get'] (ie, "crud methods")
+      //
+      isCrudMethod = [
+        'get',
+        'create',
+        'update',
+        'updateOrCreate',
+        'destroy'
+      ].some(function (method) {
+        return options.method !== method;
+      });
+
+      //
+      // Show a list of available methods
+      //
+      if (typeof _method === 'undefined') {
+        view.routes.render();
+        view.routes.present({
+          title: 'Methods Available',
+          routes: Object.keys(_resource.methods).map(function (m) {
+            return { name: m, url: '/api/' + options.resource + '/' + m };
+          })
+        }, function (err, str) {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            return res.end(JSON.stringify({
+              message: err.message
+            }, true, 2));
+          }
+
+          if (options.method) {
+            res.statusCode = 404;
+          }
+
+          res.setHeader('Content-Type', 'text/html');
+          res.end(str);
+        });
+      }
+      else {
+        //
+        // If you try to GET a method, return the schema for the method
+        //
+        if (
+          options.action === 'GET' &&
+          options.method !== 'get' &&
+          (!data.id || options.id)
+        ) {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(_method.schema, true, 2));
+        }
+        else if (typeof options.id !== 'undefined' && !isCrudMethod) {
+          _resource.methods.get(options.id, function (err, inst) {
+            if (err) {
+              return finish(err);
+            }
+            Object.keys(inst).forEach(function (p) {
+              data[p] = inst[p];
+            });
+
+            _method(data, finish);
+          });
+        }
+        else {
+
+          if (Object.keys(data).length > 0) {
+            //
+            // Do a get in order to set the status code correctly
+            //
+            if (options.method === 'updateOrCreate' && data.id) {
+              return _resource.methods.get(data.id, function (err) {
+                if (err && err.message && err.message.match('not found')) {
+                  res.statusCode = 201;
+                }
+                _method(data, finish);
+              });
+            }
+
+            //
+            // TODO: get should be able to take an options hash and not just a
+            // string. We should also have a generalized method for translating back
+            // forth and between schema-matched objects and function argument
+            // arrays. This is just a hack to make tests pass.
+            //
+            if (
+              Object.keys(data).length === 1 &&
+              data.id && (
+                options.method === 'get' ||
+                options.method === 'destroy'
+              )
+            ) {
+              data = data.id;
+            }
+
+            _method(data, finish);
+          } else {
+            _method(finish);
+          }
+
+          function finish(err, result) {
+            res.setHeader('Content-Type', 'application/json');
+
+            if (err) {
+              if (err.message && err.message.match(/not found/)) {
+                res.statusCode = 404;
+              }
+              else if (err.errors) {
+                res.statusCode = 422;
+              }
+              else {
+                res.statusCode = 500;
+              }
+
+              if (err.errors) {
+                return res.end(JSON.stringify({
+                  message: err.message,
+                  errors: err.errors
+                }, true, 2));
+              }
+              else {
+                return res.end(JSON.stringify({
+                  message: err.message
+                }, true, 2));
+              }
+            }
+
+            if (result === null) {
+              res.statusCode = 204;
+            }
+
+            if (options.method === 'create') {
+              res.statusCode = 201;
+            }
+
+            res.end(JSON.stringify(result, true, 2));
+          }
+        }
+      }
+    };
+
+    resource.http.app.get('/api', function (req, res, next) {
       view.routes.render();
       view.routes.present({
-        title: 'Methods Available',
-        routes: Object.keys(_resource.methods).map(function (m) {
-          return { name: m, url: '/api/' + options.resource + '/' + m };
+        title: 'Resources Available',
+        routes: Object.keys(resource.resources).map(function (r) {
+          return { name: r, url: '/api/' + r };
         })
       }, function (err, str) {
         if (err) {
           res.statusCode = 500;
           res.setHeader('Content-Type', 'application/json');
-          return res.end(JSON.stringify({
-            message: err.message
-          }, true, 2));
+          return res.end(JSON.stringify({ message: err.message }, true, 2));
         }
-
-        if (options.method) {
-          res.statusCode = 404;
-        }
-
         res.setHeader('Content-Type', 'text/html');
         res.end(str);
       });
-    }
-    else {
-      //
-      // If you try to GET a method, return the schema for the method
-      //
-      if (
-        options.action === 'GET' &&
-        options.method !== 'get' &&
-        (!data.id || options.id)
-      ) {
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(_method.schema, true, 2));
-      }
-      else if (typeof options.id !== 'undefined' && !isCrudMethod) {
-        _resource.methods.get(options.id, function (err, inst) {
-          if (err) {
-            return finish(err);
-          }
-          Object.keys(inst).forEach(function (p) {
-            data[p] = inst[p];
-          });
-
-          _method(data, finish);
-        });
-      }
-      else {
-
-        if (Object.keys(data).length > 0) {
-          //
-          // Do a get in order to set the status code correctly
-          //
-          if (options.method === 'updateOrCreate' && data.id) {
-            return _resource.methods.get(data.id, function (err) {
-              if (err && err.message && err.message.match('not found')) {
-                res.statusCode = 201;
-              }
-              _method(data, finish);
-            });
-          }
-
-          //
-          // TODO: get should be able to take an options hash and not just a
-          // string. We should also have a generalized method for translating back
-          // forth and between schema-matched objects and function argument
-          // arrays. This is just a hack to make tests pass.
-          //
-          if (
-            Object.keys(data).length === 1 &&
-            data.id && (
-              options.method === 'get' ||
-              options.method === 'destroy'
-            )
-          ) {
-            data = data.id;
-          }
-
-          _method(data, finish);
-        } else {
-          _method(finish);
-        }
-
-        function finish(err, result) {
-          res.setHeader('Content-Type', 'application/json');
-
-          if (err) {
-            if (err.message && err.message.match(/not found/)) {
-              res.statusCode = 404;
-            }
-            else if (err.errors) {
-              res.statusCode = 422;
-            }
-            else {
-              res.statusCode = 500;
-            }
-
-            if (err.errors) {
-              return res.end(JSON.stringify({
-                message: err.message,
-                errors: err.errors
-              }, true, 2));
-            }
-            else {
-              return res.end(JSON.stringify({
-                message: err.message
-              }, true, 2));
-            }
-          }
-
-          if (result === null) {
-            res.statusCode = 204;
-          }
-
-          if (options.method === 'create') {
-            res.statusCode = 201;
-          }
-
-          res.end(JSON.stringify(result, true, 2));
-        }
-      }
-    }
-  };
-
-  resource.http.app.get('/api', function (req, res, next) {
-    view.routes.render();
-    view.routes.present({
-      title: 'Resources Available',
-      routes: Object.keys(resource.resources).map(function (r) {
-        return { name: r, url: '/api/' + r };
-      })
-    }, function (err, str) {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        return res.end(JSON.stringify({ message: err.message }, true, 2));
-      }
-      res.setHeader('Content-Type', 'text/html');
-      res.end(str);
     });
+
+    resource.http.app.get('/api/' + options.version, function (req, res, next) {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(api, true, 2));
+    });
+
+    resource.http.app.get('/api/' + options.version + '/:resource', function (req, res, next) {
+      var r = resource.resources[req.param('resource')];
+      var obj = resource.toJSON(r);
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(obj, true, 2));
+    });
+
+    callback(null, resource.http.server);
   });
-
-  resource.http.app.get('/api/' + options.version, function (req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(api, true, 2));
-  });
-
-  resource.http.app.get('/api/' + options.version + '/:resource', function (req, res, next) {
-    var r = resource.resources[req.param('resource')];
-    var obj = resource.toJSON(r);
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(obj, true, 2));
-  });
-
-  callback(null, resource.http.server);
-
 }
 
 exports.api = api;
