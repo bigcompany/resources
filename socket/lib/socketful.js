f4568b98f8f8ccbadc20c267179a98ae11671a9a
Joshua Holbrook <josh.holbrook@gmail.com>
2013-04-09 15:06:23 -0800

    [socket] Vendored socketful, modified to use resource api

---
+++ socket/lib/socketful.js
@@ -0,0 +1,31 @@
+/*
+ * socketful.js: creates socket.io event maps to resourceful
+ *
+ * (C) 2012, Nodejitsu Inc.
+ *
+ */
+
+var socketful = exports,
+    resourceful = require('resourceful'),
+    util     = require('util'),
+    http     = require('http');
+
+exports.engines = {
+  "socket.io": require('../lib/engines/socketio')
+};
+
+//
+// ### function createServer (resources)
+// #### @resources {resourceful.Resource} Resource(s) to use for the server.
+//
+// Responds with an `http.Server` instance with a `socketfulRouter` for the
+// specified `resources`.
+//
+exports.createServer = function (resources, options, callback) {
+  options = options || {};
+  options.engine = options.engine || 'socket.io';
+  if (!Array.isArray(resources)){
+    resources = [resources];
+  }
+  return socketful.engines[options.engine].createServer(resources, options, callback);
+};
