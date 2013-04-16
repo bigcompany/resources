faa45df38d9611ae523b3005ed0509ba6e09e8d9
Marak <marak.squires@gmail.com>
2013-04-16 13:51:35 -0700

    [view] Allow for callbacks in html engine

--- view/lib/engines/html/index.js
+++ view/lib/engines/html/index.js
@@ -2,8 +2,15 @@ exports.render = function (view, data, cb) {
   var cheerio = require('cheerio'),
       str;
   if(typeof view.parent !== "undefined" && typeof view.parent.layout !== "undefined" && typeof view.parent.layout.present === 'function') {
-    var $ = cheerio.load(view.parent.layout.present());
-    $('#main').html(view.template);
+    if (cb) {
+      return view.parent.layout.present(function(err, content){
+        var $ = cheerio.load(content);
+        return cb(null, $.html());
+      });
+    } else {
+      var $ = cheerio.load(view.parent.layout.present());
+      $('#main').html(view.template);
+    }
   } else {
     var $ = cheerio.load(view.template);
   }
