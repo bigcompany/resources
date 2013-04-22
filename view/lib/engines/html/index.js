d267cd5f749f34869856557f777e2cd2dfc1dd27
Marak <marak.squires@gmail.com>
2013-04-22 14:44:40 -0700

    [view] Ensure that data is passed to layout presenter in html engine

--- view/lib/engines/html/index.js
+++ view/lib/engines/html/index.js
@@ -3,12 +3,12 @@ exports.render = function (view, data, cb) {
       str;
   if(typeof view.parent !== "undefined" && typeof view.parent.layout !== "undefined" && typeof view.parent.layout.present === 'function') {
     if (cb) {
-      return view.parent.layout.present(function(err, content){
+      return view.parent.layout.present(data, function(err, content){
         var $ = cheerio.load(content);
         return cb(null, $.html());
       });
     } else {
-      var $ = cheerio.load(view.parent.layout.present());
+      var $ = cheerio.load(view.parent.layout.present(data));
       $('#main').html(view.template);
     }
   } else {
