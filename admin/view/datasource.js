fc6f4e55fcfaac35281a090d9ff68f9ea0bf88ea
Marak Squires <marak.squires@gmail.com>
2013-05-08 00:01:46 -0700

    [admin] Cleaning up admin

--- admin/view/datasource.js
+++
@@ -1,9 +0,0 @@
-module['exports'] = function (options, callback) {
-
-  var datasource = options.datasource,
-      $ = this.$;
-
-  $('.datasource').html(JSON.stringify(datasource, true, 2));
-  return $.html();
-
-}
