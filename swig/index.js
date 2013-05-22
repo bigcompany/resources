9a63ff5707c28263ab6650bccda655ea8f0230eb
Marak Squires <marak.squires@gmail.com>
2013-05-22 03:54:29 -0700

    [swig] Add swig.configure method

--- swig/index.js
+++ swig/index.js
@@ -6,10 +6,15 @@ swig.schema.description = "for parsing swig";
 swig.method('render', render);
 
 function render (str, data) {
-  var swig = require('swig'),
-      html = swig.compile(str)(data);
+  var _swig = require('swig'),
+      html = _swig.compile(str)(data);
   return html;
 };
+
+swig.method('configure', function (options){
+  var _swig = require('swig');
+      _swig.init(options);
+});
 
 swig.dependencies = {
   "swig": "*"
