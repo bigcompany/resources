2ce52d3d045326daac18a33a84b1e5d39f1f58e6
Michael Williams <dinosaur@riseup.net>
2013-05-21 20:42:09 -0700

    bitcoin client working

--- bitcoin/test/tests.js
+++ bitcoin/test/tests.js
@@ -1,8 +1,26 @@
-var tap = require("tap"),
-    supertest = require('supertest'),
+var test = require("tap").test,
     resource = require('resource'),
-    server;
+    supertest = require('supertest'),
+    bitcoin_lib = require('bitcoin'),
+    bitcoin = resource.use('bitcoin');
 
-tap.test("start a bitcoin server", function (t) {
-  var bitcoin = resource.use('bitcoin');
+test("start a bitcoin server", function (t) {
+  bitcoin.connect({}, function(err, conn) {
+    t.notOk(err, "bitcoin client should not error");
+    t.ok(typeof conn == 'object', "bitcoin connection should be object");
+    t.ok(typeof conn.client == 'object', "bitcoin client should be object");
+    t.end();
+  });
 });
+
+
+test("generate bitcoin address", function (t) {
+  bitcoin.connect({}, function(err, conn) {
+    conn.client.getNewAddress(function (err, address) {
+      t.notOk(err, "getNewAddress() should not error");
+      t.ok(typeof address == 'string', "address should be string");
+      t.end();
+    });
+  });
+});
+
