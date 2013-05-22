21892284fa6787468903e6b338103fa98173a6a0
Michael Williams <dinosaur@riseup.net>
2013-05-22 00:36:39 -0700

    added almost working bitcoin methods

--- bitcoin/index.js
+++ bitcoin/index.js
@@ -1,6 +1,7 @@
 var resource = require('resource'),
     bitcoin_lib = require('bitcoin'),
-    bitcoin = resource.define('bitcoin');
+    bitcoin = resource.define('bitcoin'),
+    commands = require(require.resolve('bitcoin') + '/../commands');
 
 bitcoin.schema.description = "for managing bitcoins";
 
@@ -53,14 +54,16 @@ bitcoin.method('connect', connect, {
   }
 });
 function connect(options, callback) {
-  var tuple = [options.host, options.port, options.user].join(':');
+  var connection_id = bitcoin.connection_id(options);
       Client = bitcoin_lib.Client; // bitcoin client class
 
   // lookup table of bitcoin connections
-  bitcoin.connections[tuple] = {};
+  bitcoin.connections[connection_id] = {
+    id: connection_id
+  };
 
   var client; // bitcoin client instance
-  client = bitcoin.connections[tuple].client = new Client(options);
+  client = bitcoin.connections[connection_id].client = new Client(options);
   // check client status through getInfo()
   client.getInfo(function(err, info) {
     // check for possible errors
@@ -71,9 +74,45 @@ function connect(options, callback) {
       return callback(info.errors, null);
     }
     // if no errors, return client
-    return callback(null, bitcoin.connections[tuple]);
+    return callback(null, bitcoin.connections[connection_id]);
   });
 }
+
+bitcoin.method('connection_id', connection_id, {
+  description: 'converts connect info to connection_id',
+  properties: {
+    options: {
+      type: 'object',
+      properties: {
+        host: bitcoin.schema.properties.server.properties.host,
+        port: bitcoin.schema.properties.server.properties.port,
+        user: bitcoin.schema.properties.server.properties.user
+      }
+    },
+    callback: {
+      type: 'function'
+    }
+  }
+});
+function connection_id(options, callback) {
+  return [options.host, options.port, options.user].join(':');
+}
+
+for (command in commands) {
+  // define resource method for library command
+  console.log(command);
+  bitcoin.method(command, function(connection_id, args, callback) {
+    console.log(command);
+    // get client for connection_id
+    client = bitcoin.connections[connection_id].client;
+    // add callback to end of args
+    args.push(callback);
+    // call client command with args
+    client[command].apply(client, args);
+  });
+};
+
+console.log(bitcoin);
 
 exports.bitcoin = bitcoin;
 exports.dependencies = {
--- bitcoin/test/tests.js
+++ bitcoin/test/tests.js
@@ -8,6 +8,7 @@ test("start a bitcoin server", function (t) {
   bitcoin.connect({}, function(err, conn) {
     t.notOk(err, "bitcoin client should not error");
     t.ok(typeof conn == 'object', "bitcoin connection should be object");
+    t.ok(typeof conn.id == 'string', "bitcoin connection_id should be string");
     t.ok(typeof conn.client == 'object', "bitcoin client should be object");
     t.end();
   });
@@ -16,7 +17,8 @@ test("start a bitcoin server", function (t) {
 
 test("generate bitcoin address", function (t) {
   bitcoin.connect({}, function(err, conn) {
-    conn.client.getNewAddress(function (err, address) {
+    bitcoin.getNewAddress(conn.id, [], function (err, address) {
+      console.log(err);
       t.notOk(err, "getNewAddress() should not error");
       t.ok(typeof address == 'string', "address should be string");
       t.end();
