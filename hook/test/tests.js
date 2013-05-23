6768de8f42849cc5f065d9d6911e95c423205801
Joshua Holbrook <josh.holbrook@gmail.com>
2013-05-22 22:13:26 -0800

    [hook] [test] A number of new asserts (some are failing)

--- hook/test/tests.js
+++ hook/test/tests.js
@@ -12,12 +12,32 @@ counter.property('timestamp', { type: 'any' });
 
 counter.persist('memory');
 
+counter.before('create', function (c, cb) {
+  c.timestamp = new Date();
+  cb(null, c);
+});
+
 creature.method('poke', function (text, cb) {
   if (!cb) {
     cb = text;
     text = 'poked!';
   }
   cb(null, text);
+});
+
+counter.after('create', function (c, cb) {
+  console.log('# created counter ' + c.id);
+  cb(null, c);
+});
+
+creature.after('create', function (c, cb) {
+  console.log('# created creature ' + c.id);
+  cb(null, c);
+});
+
+hook.after('create', function (h, cb) {
+  console.log('# created hook ' + h.id);
+  cb(null, h);
 });
 
 test("create hooks",function (t) {
@@ -44,9 +64,9 @@ test("start hooks", function (t) {
 
 test("create creatures", function (t) {
   var creatures = [
-    { id: 'korben', life: 10, type: 'dragon' },
+    { id: 'korben', life: 15, type: 'dragon' },
     { id: 'hazel', life: 15, type: 'dragon' },
-    { id: 'booboo', life: 10, type: 'unicorn' }
+    { id: 'booboo', life: 15, type: 'unicorn' }
   ];
 
   t.plan(3);
@@ -66,19 +86,51 @@ test("call poke", function (t) {
 });
 
 test("hooks fired", function (t) {
+
+  t.plan(18);
+
+  hook.all(function (err, hooks) {
+    t.error(err, 'got all hooks');
+    t.equal(hooks.length, 3, 'three hooks total');
+  });
+
+  hook.find({ if: "creature::poke" }, function (err, hooks) {
+    t.error(err, 'got hooks for creature::poke');
+    t.equal(hooks.length, 2, 'two hooks for creature::poke');
+  });
+
+  hook.find({ then: "counter::create" }, function (err, hooks) {
+    t.error(err, 'got hooks triggering counter::create');
+    t.equal(hooks.length, 2, 'two hooks triggering counter::create');
+  });
+
   creature.all(function (err, creatures) {
     t.error(err, 'got all creatures');
-    counter.all(function (err, counters) {
-      t.error(err, 'got all counters');
-      hook.all(function (err, hooks) {
-        t.error(err, 'got all hooks');
+    t.equal(creatures.length, 4, 'four creatures total');
+  });
 
-        t.equal(creatures.length, 4, 'four creatures');
-        t.equal(counters.length, 4, 'four counters');
-        t.equal(hooks.length, 3, 'three hooks');
+  creature.find({ life: 15 }, function (err, creatures) {
+    t.error(err, 'got creatures with life 15');
+    t.equal(creatures.length, 3, 'three creatures with life 15');
+  });
 
-        t.end();
-      });
-    });
+  creature.find({ life: 100 }, function (err, creatures) {
+    t.error(err, 'got creatures with life 100');
+    t.equal(creatures.length, 1, 'one creature with life 100');
+  });
+
+  counter.all(function (err, counters) {
+    t.error(err, 'got all counters');
+    t.equal(counters.length, 4, 'four counters total');
+  });
+
+  counter.find({ message: 'creature create' }, function (err, counters) {
+    t.error(err, 'got counters from creature::create');
+    t.equal(counters.length, 3, 'three counters from creature::create');
+  });
+
+  counter.find({ message: 'creature poke' }, function (err, counters) {
+    t.error(err, 'got counters from creature::poke');
+    t.equal(counters.length, 1, 'one counter from creature::poke');
   });
 });
