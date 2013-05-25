41f0e0157d28aa440346ae6b98a8766062120f81
Joshua Holbrook <josh.holbrook@gmail.com>
2013-05-24 17:23:50 -0800

    [mesh] Examples expliticly use http server and set port

--- mesh/example/server.js
+++ mesh/example/server.js
@@ -1,19 +1,26 @@
 var resource = require('resource');
-
+var http = resource.use('http');
 var mesh = resource.use('mesh');
 
-mesh.listen(function(){
+http.listen(function (err) {
+  if (err) {
+    throw err;
+  }
+  mesh.listen(function(err){
+    if (err) {
+      throw err;
+    }
 
-  mesh.onAny(function(data){
-    mesh.emit('server-echo-' + this.event, data);
+    mesh.onAny(function(data){
+      mesh.emit('server-echo::' + this.event, data);
+    });
+
+    setInterval(function(){
+      mesh.emit('server-foo', { bar: "foo" });
+    }, 2000);
+
+    mesh.onAny(function(data){
+      console.log(this.event, data)
+    })
   });
-
-  setInterval(function(){
-    mesh.emit('server-foo', { bar: "foo" });
-  }, 2000);
-
-  mesh.onAny(function(data){
-    console.log(this.event, data)
-  })
-
 });
