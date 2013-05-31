844dfd4c3a5f96c09739aa5abe5811446bee69d3
Michael Williams <dinosaur@riseup.net>
2013-05-31 02:57:32 -0700

    [wallet] [inprogress] worked on wallet creation and subsequent polling of servers for transactions; outlined transaction and transfer resources

---
+++ transfer/test/tests.js
@@ -0,0 +1,8 @@
+var test = require('tap').test,
+    resource = require('resource'),
+    decimal = require('decimal'),
+    transfer = resource.use('transfer');
+
+test('start()', function (t) {
+  t.end();
+});
