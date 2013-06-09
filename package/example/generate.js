22713c8a59429338ad743c6535b8251b8540f422
Michael Williams <dinosaur@riseup.net>
2013-06-09 16:45:51 -0700

    [package] fixed build method

--- package/example/generate.js
+++
@@ -1,6 +0,0 @@
-var resource = require('resource'),
-    package = resource.use('package');
-
-package.generate(process.argv[2], function(err, result) {
-  resource.logger.info(JSON.stringify(result));
-});
