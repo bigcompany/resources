672eeba40aef3b2809594aaf15bafbc61abb380c
Marak <marak.squires@gmail.com>
2013-07-09 02:11:20 -0700

    [repl] [fix] Bind resource scope to repo, not big.

--- repl/index.js
+++ repl/index.js
@@ -3,10 +3,13 @@ repl = resource.define('repl');
 
 repl.schema.description = "enables an interactive Read-Eval-Print-Loop ( REPL )";
 
-repl.method('start', function() {
+repl.method('start', function(callback) {
   resource.logger.warn('STDIN will now be processed in a Read-Eval-Print-Loop');
-  resource.logger.help('try console logging the "big" object');
-  require('repl').start("> ").context.big = resource;
+  resource.logger.help('try console logging the "resource" object');
+  require('repl').start("> ").context.resource = resource;
+  if (callback) {
+    callback(null, 'started');
+  }
 });
 
 exports.repl = repl;
