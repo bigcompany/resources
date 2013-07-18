018d8e2e7f1f456f92f2629ac0f522d891ac7ab7
Michael Williams <dinosaur@riseup.net>
2013-07-18 00:11:34 -0700

    [jugglingdb] resource.start not necessary at this point

--- jugglingdb/index.js
+++ jugglingdb/index.js
@@ -29,58 +29,56 @@ function enable (r, options) {
   var _type = options.type || 'fs';
   resource.use(options.type);
   crud(r);
-  resource[options.type].start(function() {
-    //
-    // Map uuid library to jugglingdb resource
-    //
-    var uuid = require('node-uuid');
-    jugglingdb.uuid = uuid;
-    var schema = new Schema(_type, {
-      database: options.name || "big",
-      host: options.host,
-      port: options.port,
-      path: options.path || path.join(resource.helper.appDir, 'db'),
-      username: options.username,
-      password: options.password,
-      options: options.options,
-      https: true // TODO: check that HTTPS actually does something
-    });
-
-    //
-    // Create empty schema object for mapping between resource and JugglingDB
-    //
-    var _schema = {};
-
-    //
-    // For every property in the resource schema, map the property to JugglingDB
-    //
-    Object.keys(r.schema.properties).forEach(function(p){
-      var prop = r.schema.properties[p];
-      _schema[p] = { type: jugglingType(prop) };
-    });
-    function jugglingType(prop) {
-      var typeMap = {
-        'string': String,
-        'number': Number,
-        'integer': Number,
-        'array': JSON,
-        'boolean': Boolean,
-        'object': JSON,
-        'null': null,
-        'any': String
-      };
-
-      return typeMap[prop.type] || String;
-    }
-
-    //
-    // Create a new JugglingDB schema based on temp schema
-    //
-    var Model = schema.define(r.name, _schema);
-
-    // assign model to resource
-    r.model = Model;
+  //
+  // Map uuid library to jugglingdb resource
+  //
+  var uuid = require('node-uuid');
+  jugglingdb.uuid = uuid;
+  var schema = new Schema(_type, {
+    database: options.name || "big",
+    host: options.host,
+    port: options.port,
+    path: options.path || path.join(resource.helper.appDir, 'db'),
+    username: options.username,
+    password: options.password,
+    options: options.options,
+    https: true // TODO: check that HTTPS actually does something
   });
+
+  //
+  // Create empty schema object for mapping between resource and JugglingDB
+  //
+  var _schema = {};
+
+  //
+  // For every property in the resource schema, map the property to JugglingDB
+  //
+  Object.keys(r.schema.properties).forEach(function(p){
+    var prop = r.schema.properties[p];
+    _schema[p] = { type: jugglingType(prop) };
+  });
+  function jugglingType(prop) {
+    var typeMap = {
+      'string': String,
+      'number': Number,
+      'integer': Number,
+      'array': JSON,
+      'boolean': Boolean,
+      'object': JSON,
+      'null': null,
+      'any': String
+    };
+
+    return typeMap[prop.type] || String;
+  }
+
+  //
+  // Create a new JugglingDB schema based on temp schema
+  //
+  var Model = schema.define(r.name, _schema);
+
+  // assign model to resource
+  r.model = Model;
 }
 
 //
