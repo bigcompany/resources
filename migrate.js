953a2e2879521df0ee4714035d1fe10c33d05cfc
Thanasis Polychronakis <thanpolas@gmail.com>
2013-12-17 21:34:26 +0200

    scaffold migration script

---
+++ migrate.js
@@ -0,0 +1,7 @@
+/**
+ * @fileOverview Migration script to resource 0.5.x, this script will:
+ *   - create a new branch filtering the selected folder (resource)
+ *   - create a new repo pulling this branch in a new directory
+ *   - apply OSS boilerplate
+ */
+
